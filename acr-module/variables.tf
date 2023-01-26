d9286b95d6e0aedb13806f1da7e643642fba931c
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-01-26 15:50:16 +0530

    Create variables.tf

---
+++ acr-module/variables.tf
@@ -0,0 +1,15 @@
+variable "acr_name" {}
+
+variable "resource_group_name" {}
+
+variable "location" {}
+
+variable "private_zone_id" {}
+
+variable "virtual_network_name" {
+  type = string
+}
+
+variable "acr_subnet_name" {
+  type = string
+}
