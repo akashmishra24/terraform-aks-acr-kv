e1bde512ab30b30b2373e6b00e8080c9cc3f00ab
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-01-26 15:49:53 +0530

    Create acr.tf

---
+++ acr-module/acr.tf
@@ -0,0 +1,42 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_subnet" "snet" {
+  name                 = var.acr_subnet_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                          = var.acr_name
+  resource_group_name           = data.azurerm_resource_group.rg.name
+  location                      = data.azurerm_resource_group.rg.location
+  sku                           = "Premium"
+  public_network_access_enabled = false
+  admin_enabled                 = true
+}
+
+resource "azurerm_private_endpoint" "acr-endpoint" {
+  name                = "${var.acr_name}-to_aks"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  subnet_id           = data.azurerm_subnet.snet.id
+
+  private_service_connection {
+    name                           = "${var.acr_name}-privateserviceconnection"
+    private_connection_resource_id = azurerm_container_registry.acr.id
+    subresource_names              = ["registry"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "acr-endpoint-zone"
+    private_dns_zone_ids = [var.private_zone_id]
+  }
+}
