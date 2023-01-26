ec3450aaa3491a23053d79b6cb36219718762042
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-01-26 13:01:04 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-iac-cox-poc-01"
+    storage_account_name = "tfstatedemo1"
+    container_name       = "tfstate"
+    key                  = "terraform_aks_poc.tfstate"
+  }
+}
+
+terraform {
+  required_version = "1.3.5"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.35.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
