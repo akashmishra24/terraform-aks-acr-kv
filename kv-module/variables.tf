db9a8e480c12dae4dee01c9ab1664c4d41a4de30
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-01-26 15:51:25 +0530

    Create variables.tf

---
+++ kv-module/variables.tf
@@ -0,0 +1,17 @@
+variable "private_zone_id" {}
+
+variable "key_vault_name" {}
+
+variable "resource_group_name" {}
+
+variable "virtual_network_name" {
+  type = string
+}
+
+variable "acr_subnet_name" {
+  type = string
+}
+
+variable "tenant_id" {
+
+}
